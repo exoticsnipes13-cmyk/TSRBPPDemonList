a9eb6cbe555d5e82d5cc190a7e874dc061c44237
exoticsnipes13-cmyk <exoticsnipes13@gmail.com>
2026-05-02 15:11:17 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-58.2441*Math.pow(rank-1, 0.4) + 350) *
+    let score = (-58.2441*Math.pow(rank-2, 0.8) + 350) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
